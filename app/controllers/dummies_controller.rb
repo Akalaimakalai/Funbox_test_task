a737a12f1691eb6159720c788a1ba3a4fde8b648
Ar4i <jackshaakara@gmail.com>
2020-03-25 19:42:24 +0300

    dummies_controller work with cookies on :create

--- app/controllers/dummies_controller.rb
+++ app/controllers/dummies_controller.rb
@@ -1,11 +1,13 @@
 class DummiesController < ApplicationController
 
   def new
-    @dummy = Dummy.new
+    @dummy = Dummy.new(value: cookies[:last_value], deadline: cookies[:last_deadline])
   end
 
   def create
     @dummy = Dummy.create(dummy_params)
+    cookies[:last_value] = @dummy.value
+    cookies[:last_deadline] = @dummy.deadline
   end
 
   private
