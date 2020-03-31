c424fc073e76ca5ec174b2f7f998407f3e23a046
Ar4i <jackshaakara@gmail.com>
2020-03-31 22:10:46 +0300

    travis-015

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+ExchangeRate.create(value: DailyRateService.call, char_code: "USD", name: "Доллар США")
+
+puts "USD created"
