b2a604ae9d36925a1197efa856cfa77e7200cb84
Ar4i <jackshaakara@gmail.com>
2020-04-02 18:16:56 +0300

    websockets corrections

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # === MY CONFIGS ===
+  config.action_cable.allowed_request_origins = [Rails.application.credentials[Rails.env.to_sym][:FQDN]]
+  # ---END---
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
